980e4b758d56d245f8bea5c8f824d5052a17384d
Shaun Le Dorlot <shaun.le-dorlot@etu.u-pec.fr>
2021-04-17 20:30:55 +0200

    Suite Tests

---
+++ puzzlecityclient/commons/TestJson.java
@@ -0,0 +1,168 @@
+package commons;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import commons.JSONSocket;
+//import view.Dashboard;
+import commons.AccessServer;
+
+public class TestJson {	
+	private static JSONSocket client = new JSONSocket();
+	public static Connection c; 
+	private static String URL = "jdbc:postgresql://172.31.249.135:5432/dettecitydb";
+	private static String login = "toto" ;
+	private static String password = "toto";
+
+
+	public static Connection createConnection() throws SQLException {
+		try {
+
+			return  DriverManager.getConnection (URL, login, password);
+		} catch (SQLException e) {
+			throw new SQLException("Can't create connection", e);
+		}
+
+	}
+	
+	public static void main(String [] args) throws SQLException, IOException {
+	//	TestJson t = new TestJson();
+	//	Dashboard page = new Dashboard();
+	//	page.frame.setVisible(true);
+	
+		
+	}
+	
+	
+	
+	/* function used to launch the cars simulation when the server receive a launch simulation 
+	 demand it starts the thread that insert the cars in tables to increase or decrease cars in town  */
+	
+	public static org.json.simple.JSONObject launchSimulation() throws SQLException, IOException,UnsupportedEncodingException, JSONException, InterruptedException {
+		System.out.println("je rentre deja dans la simulation");
+		client = new JSONSocket();
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		obj.put("demandType", String.valueOf("launchSimulation")); 
+		System.out.println(obj);
+		Thread.sleep(2000); 
+		org.json.simple.JSONObject reponseSimulation = JSONSocket.sendMessage(obj);
+		System.out.println(reponseSimulation);
+		client.stopConnection(); 
+
+		return reponseSimulation; 
+		
+	}
+	
+	/* method called when the program starts it calls the server to get all the bornes states and
+	 * also the initial number of cars actually in town and the initial max in the server */
+	
+	public static org.json.simple.JSONObject getBornes() throws SQLException, IOException,UnsupportedEncodingException, JSONException {
+		JSONSocket client = new JSONSocket();
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		System.out.println("connexion réussie");
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		System.out.println("envoi du demandtype");
+		obj.put("demandType",String.valueOf("getInitialInfos")); 
+		System.out.println(obj);	
+		org.json.simple.JSONObject reponseBornes = client.sendMessage(obj);
+		System.out.println(reponseBornes);
+		client.stopConnection(); 
+
+		return reponseBornes; 
+		
+	}
+	
+	/* method that takes in parameter the new max vehicules authorised in town  and updates the 
+	 * the table with it by replacing the old one*/
+	
+	public static org.json.simple.JSONObject changeMax(int max) throws SQLException, IOException,UnsupportedEncodingException, JSONException {
+		client = new JSONSocket();
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		obj.put("demandType",String.valueOf("vehiculeThreshold")); 
+		obj.put("maxCars",Integer.valueOf(max)); 
+		System.out.println(obj);	
+		org.json.simple.JSONObject reponseMaxVehicules = client.sendMessage(obj);
+		System.out.println(reponseMaxVehicules);
+		client.stopConnection(); 
+
+		return reponseMaxVehicules; 
+		
+	}
+	
+	/*************************** static method that sends a demand via the socket to the server to 
+	rise the bornes by changing their state 
+	 * @throws JSONException ***********************/	
+	
+	public static org.json.simple.JSONObject riseBornes() throws SQLException, IOException,UnsupportedEncodingException, JSONException {
+		System.out.println("l'état des bornes va etre modifié");
+		client = new JSONSocket();
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		obj.put("demandType",String.valueOf("RiseBornes")); 
+		System.out.println(obj);	
+		org.json.simple.JSONObject reponseSimulation = client.sendMessage(obj);
+		System.out.println(reponseSimulation);
+		client.stopConnection(); 
+
+		return reponseSimulation; 
+		
+	}
+	
+	/*************************** static method that sends a demand via the socket to the server to 
+	lower the bornes by changeing their state 
+	 * @throws JSONException ***********************/
+
+	public static org.json.simple.JSONObject LowerBornes() throws SQLException, IOException,UnsupportedEncodingException, JSONException {
+			System.out.println("l'état des bornes va etre modifié");
+			client = new JSONSocket();
+			client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+			JSONObject obj=new JSONObject();  //JSONObject creation
+			obj.put("demandType",String.valueOf("LowerBornes")); 
+			System.out.println(obj);	
+			org.json.simple.JSONObject reponseSimulation = client.sendMessage(obj);
+			System.out.println(reponseSimulation);
+			client.stopConnection(); 
+
+			return reponseSimulation; 
+
+	}
+	
+	/* static method that takes in parameter the filters selected by the user and send them to the user
+	 * in order to get from the history table the cars corresponding to the criteria 
+	 */
+	
+	public static org.json.simple.JSONObject searchVehicule(String dateDebut, String dateFin, String zone, String type) throws SQLException, IOException,UnsupportedEncodingException, JSONException {
+		System.out.println("je rentre deja dans la recherche vehicules");
+		client = new JSONSocket();
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		obj.put("demandType",String.valueOf("filterVehicule"));
+		if(type.equals("Les deux")) {
+			type = "town";
+		}
+		if(zone.equals("toute la ville")) {
+			zone = "All";
+		}
+		obj.put("type", String.valueOf(type));
+		obj.put("zone", String.valueOf(zone));
+		obj.put("dateDebut", String.valueOf(dateDebut));
+		obj.put("dateFin", String.valueOf(dateFin));
+		
+		System.out.println(obj);	
+		org.json.simple.JSONObject reponseSearch = client.sendMessage(obj);
+		client.stopConnection(); 
+
+		return reponseSearch; 
+		
+	}
+	
+	
+}
---
+++ puzzlecityclient/tests/TestsCLI.java
@@ -0,0 +1,118 @@
+package tests;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+
+import commons.JSONSocket;
+import commons.AccessServer;
+
+import indicator.CarIndicator;
+import indicator.SensorIndicator;
+
+public class TestsCLI {
+	private JSONSocket client = new JSONSocket();
+	public static Connection c; 
+	private static String URL = "jdbc:postgresql://172.31.249.44:5432/NamaiDB";
+	private static String login = "toto" ;
+	private static String password = "toto";
+
+
+	public static Connection createConnection() throws SQLException {
+		try {
+
+			return  DriverManager.getConnection (URL, login, password);
+		} catch (SQLException e) {
+			throw new SQLException("Can't create connection", e);
+		}
+
+	}
+	
+	public static void main(String [] args) throws SQLException, IOException {
+		TestsCLI t = new TestsCLI();
+		t.technicalTests();
+	//	t.testCLI();
+		
+		
+	//	PagePrincipale page = new PagePrincipale();
+	//	page.setVisible(true);
+	
+		
+	}
+	
+	public void technicalTests() throws IOException {
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		JSONObject obj=new JSONObject();  //JSONObject creation
+		System.out.println("***************** TEST 1 : Insertion nouveaux max voitures + affichage*********************");
+		obj.put("demandType",String.valueOf("ChangeLimitTest")); 
+		System.out.println(obj);	
+		JSONObject ChangeLimitTest = client.sendMessage2(obj);
+		System.out.println("voici les resultats des tests :" + ChangeLimitTest);
+		
+		ArrayList<JSONObject> allValues = new ArrayList<JSONObject>();// Creation d'un tableau de type SensorIndicator
+		allValues = (ArrayList<JSONObject>) ChangeLimitTest.get("testsResults");
+		for(int i = 0; i<allValues.size();i++) { // Creating a loop to display all sensors in the table sensors
+			System.out.println("reponse: "+allValues.get(i).get("reponse")+
+					" | max voitures: "+allValues.get(i).get("max voitures: ")); 
+			
+		}
+		client.stopConnection(); 
+		
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		obj=new JSONObject();
+		System.out.println("***************** TEST 2 : Test rechreche et affichage vehicules selon filtres*********************");
+		obj.put("demandType",String.valueOf("selectHistoryTest")); 
+		System.out.println(obj);	
+		JSONObject selectInHistorytest = client.sendMessage2(obj);
+		System.out.println("voici les resultats des tests :" + selectInHistorytest);
+		allValues = (ArrayList<JSONObject>) selectInHistorytest.get("testsResults");
+		for(int i = 0; i<allValues.size();i++) { // Creating a loop to display all sensors in the table sensors 
+			System.out.println("************** rechreche  " + i + " *********************");
+			ArrayList<JSONObject> allCars = (ArrayList<JSONObject>) allValues.get(i).get("voitures");
+			if(allCars.size() == 0) {
+				System.out.println("la rechreche ne retourne pas de resultats");
+			}
+			for(int j = 0; j<allCars.size(); j++) {
+					
+					System.out.println("voiture: " + allCars.get(j).get("vehicule") +
+										" | date: "   + allCars.get(j).get("date") +
+										" | type: "   + allCars.get(j).get("type") +
+										" | position: " + allCars.get(j).get("position"));
+			
+			}
+
+		}
+		client.stopConnection(); 
+		
+		client.startConnection(AccessServer.getSERVER(), AccessServer.getPORT_SERVER());
+		obj=new JSONObject();
+		System.out.println("***************** TEST 3 : insertion voitures et insertion nouveau nb vehicules*********************");
+		obj.put("demandType",String.valueOf("insertCarsAndActualNb")); 
+		System.out.println(obj);	
+		JSONObject InsertCarsAndNbVoitures = client.sendMessage2(obj);
+		System.out.println("voici les resultats des tests :" + InsertCarsAndNbVoitures);
+		
+		ArrayList<JSONObject> InsertedCars = (ArrayList<JSONObject>) InsertCarsAndNbVoitures.get("testsResults");
+		for(int j = 0; j<InsertedCars.size(); j++) {
+				
+				System.out.println("reponse: " + InsertedCars.get(j).get("reponse") +
+									" | nombre de vehicules: "   + InsertedCars.get(j).get("NbVoitures") +
+									" | vehicule: "   + InsertedCars.get(j).get("vehicule") +
+									" | type: "   + InsertedCars.get(j).get("type"));
+		
+		}
+		client.stopConnection(); 
+	}
+	
+	
+}
+	
+
